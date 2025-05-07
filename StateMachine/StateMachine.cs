57d74e2bd5d2d505518d3b58a20908142c9fbdc8
BNJMO <bnjmo.med@gmail.com>
2025-05-07 16:08:46 +0100

    Extend state machine with RevertState method and debug log

--- StateMachine/StateMachine.cs
+++ StateMachine/StateMachine.cs
@@ -1,31 +1,81 @@
 ﻿using System.Collections;
 using System;
 using System.Collections.Generic;
+using BNJMO;
 using UnityEngine;
 
-
-public class StateMachine<T> where T : Enum
+namespace BNJMO
 {
-    public StateMachineHandler<T> Handler { get; private set; } = new StateMachineHandler<T>();
-	
-    public T CurrentState { get; private set; }
-    public T PreviousState { get; private set; }
-	
-    public void UpdateState(T newState)
+    public class StateMachine<T> where T : Enum
     {
-        if (EqualityComparer<T>.Default.Equals(CurrentState, newState))
-            return;
+        #region Public Events
+
+
+        #endregion
+
+        #region Public Methods
         
-        PreviousState = CurrentState;
-        CurrentState = newState;
-        Handler.Update(CurrentState, PreviousState);
-    }
+        public void UpdateState(T newState)
+        {
+            if (EqualityComparer<T>.Default.Equals(CurrentState, newState))
+                return;
+        
+            PreviousState = CurrentState;
+            CurrentState = newState;
+            Handler.Update(CurrentState, PreviousState);
+
+            if (DebugStateChange)
+            {
+                Debug.Log($"State updated from {PreviousState} to {CurrentState}");
+            }
+        }
     
-    public void RevertState()
-    {
-        T temp = CurrentState;
-        CurrentState = PreviousState;
-        PreviousState = temp;
-        Handler.Update(CurrentState, PreviousState);
+        public void RevertState()
+        {
+            (CurrentState, PreviousState) = (PreviousState, CurrentState);
+            Handler.Update(CurrentState, PreviousState);
+            
+            if (DebugStateChange)
+            {
+                Debug.Log($"State updated from {PreviousState} to {CurrentState}");
+            }
+        }
+        
+        #endregion
+
+        #region Inspector Variables
+
+
+        #endregion
+
+        #region Variables
+        
+        public StateMachineHandler<T> Handler { get; private set; } = new StateMachineHandler<T>();
+
+        public T CurrentState { get; private set; }
+        
+        public T PreviousState { get; private set; }
+        
+        public bool DebugStateChange { get; set; }
+        
+        private Dictionary<NetcodePlayerListener, ENetworkID> playerListenersMap = new();
+
+        #endregion
+
+        #region Life Cycle
+
+    
+        #endregion
+
+        #region Events Callbacks
+
+
+        #endregion
+
+        #region Others
+
+
+
+        #endregion
     }
 }
